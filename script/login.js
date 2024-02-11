48b63bdcbb015ced4aadfd636134cab1388e536b
Meet5555 <bhimanimeet55555@gmail.com>
2024-02-11 08:39:54 +0530

    added toasts in login page and responsiveness of ::after in nav-link

--- script/login.js
+++ script/login.js
@@ -9,12 +9,23 @@ loginForm.addEventListener("submit", function (event) {
   const enteredPassword = passwordInput.value;
 
   if (enteredUsername.toString().trim().length == 0 || enteredPassword.toString().trim().length == 0) {
-    alert("Must enter username and password");
+    // alert("Must enter username and password");
+    Toastify({
+      text: "Please enter both username and password correctly",
+      duration: 3000,
+      close: true,
+      gravity: "top",
+      position: "center",
+      stopOnFocus: true,
+      style: {
+        background: "rgb(252, 90, 90)",
+      }
+    }).showToast();
     resetForm();
     return;
   }
 
-  // Taking the data from LS
+  // Taking the data from Local storage
   const usersData = JSON.parse(localStorage.getItem("users")) || [];
 
   // Checking if the entered username is valid
@@ -29,12 +40,34 @@ loginForm.addEventListener("submit", function (event) {
       localStorage.setItem("userObj", JSON.stringify(user));
       window.location.href = "/index.html";
     } else {
-      alert("Invalid Credentials. Please try again.");
+      // alert("Invalid Credentials. Please try again.");
+      Toastify({
+        text: "Invalid Credentials. Please try again",
+        duration: 3000,
+        close: true,
+        gravity: "top",
+        position: "center",
+        stopOnFocus: true,
+        style: {
+          background: "rgb(252, 90, 90)",
+        }
+      }).showToast();
       resetForm();
       return;
     }
   } else {
-    alert("User not Found, Register First.");
+    // alert("User not Found, Register First.");
+    Toastify({
+      text: "User not Found, Register First",
+      duration: 3000,
+      close: true,
+      gravity: "top",
+      position: "center",
+      stopOnFocus: true,
+      style: {
+        background: "rgb(255, 232, 36)",
+      }
+    }).showToast();
     resetForm();
     return;
   }
